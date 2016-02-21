39011cc389c1a36fc3dd32d1a9ad4292e5b4e29f
Wegrim <atwegrim@gmail.com>
2016-02-21 21:32:22 +0300

    Complete lesson 15

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,15 +13,9 @@ namespace Snake
         {           
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
-            
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();           
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -33,7 +27,12 @@ namespace Snake
             
             while(true)
             {
-                if(snake.Eat(food))
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
+                if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
